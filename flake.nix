a55e412c637cd4aef123b712876155a34aa4fde8
Greg Pfeil <greg@technomadic.org>
2025-10-27 09:46:54 -0600

    Add a Nix flake

---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "Keystone dev environment";
+
+  nixConfig = {
+    allow-import-from-derivation = true;
+    extra-substituters = ["https://nix-community.cachix.org"];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    sandbox = "relaxed";
+    use-registry = false;
+  };
+
+  outputs = {
+    fenix,
+    flake-utils,
+    nixpkgs,
+    pyproject,
+    self,
+    systems,
+  }:
+    flake-utils.lib.eachSystem (import systems) (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      apps =
+        builtins.mapAttrs (_: drv: flake-utils.lib.mkApp {inherit drv;}) self.packages.${system};
+
+      packages = let
+        simulator = import ./nix/packages/simulator.nix {
+          inherit (nixpkgs) lib;
+          inherit fenix pkgs pyproject;
+        };
+      in
+        {default = self.packages.${system}.multi-coins-simulator;}
+        // nixpkgs.lib.mapAttrs'
+        (name: arg: nixpkgs.lib.nameValuePair "${name}-simulator" (simulator name arg))
+        ## These are the `FEATURE_VARIANT`s from ./rust/CMakeLists.txt.
+        {
+          btc-only = "btc_only";
+          cypherpunk = "cypherpunk";
+          multi-coins = "general";
+        };
+
+      devShells.default = pkgs.mkShell {
+        inputsFrom = nixpkgs.lib.attrValues self.packages.${system};
+        packages = [
+          pkgs.alejandra
+        ];
+        ## TODO: This is because we can’t bypass a submodule with the flake’s
+        ##       `submodules = true`.
+        shellHook = ''
+          ${nixpkgs.lib.getExe pkgs.git} \
+            -c submodule.keystone3-firmware-release.update=none \
+            submodule update --init --recursive
+        '';
+      };
+
+      formatter = pkgs.alejandra;
+    });
+
+  inputs = {
+    fenix = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/fenix";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
+    pyproject = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:pyproject-nix/pyproject.nix";
+    };
+    ## FIXME: Currently need to manually update the submodules (see NixOS/nix#14245).
+    ##     > git -c submodule.keystone3-firmware-release.update=none submodule update --init --recursive
+    # self.submodules = true; # ["external/ctaes" "rust-crypto-core"];
+    systems.follows = "flake-utils/systems";
+  };
+}
---
+++ nix/packages/simulator.nix
@@ -0,0 +1,81 @@
+{
+  fenix,
+  lib,
+  pkgs,
+  pyproject,
+}: let
+  projectRoot = ../..;
+  ## Read Python environment from requirements.txt.
+  pythonEnv =
+    pkgs.python3.withPackages
+    ((pyproject.lib.project.loadRequirementsTxt {inherit projectRoot;}).renderers.withPackages {
+      python = pkgs.python3;
+    });
+  ## Read Rust environment from rust-toolchain.
+  ##
+  ## TODO: I think this can only build for the simulator, because `thumbv7em-none-eabihf` is not
+  ##       a supported target for Fenix.
+  rustEnv = fenix.packages.${pkgs.system}.fromToolchainFile {
+    file = "${projectRoot}/rust-toolchain";
+    sha256 = "yN9i4c4NJ69YywO3PxRJ/HtB5IWjDUzH/UWSOSLq29A=";
+  };
+in
+  name: firmware: let
+    pname = "keystone-${name}-simulator";
+  in
+    pkgs.stdenv.mkDerivation {
+      inherit pname;
+      version = "2.2.14";
+      src = projectRoot;
+
+      ## This can’t be sandboxed, because it uses Cargo to fetch dependencies.
+      __noChroot = true;
+
+      buildInputs = [pkgs.SDL2];
+
+      nativeBuildInputs = [
+        pkgs.cacert ## SSL certificates (wouldn’t need if this were sandboxed)
+        pkgs.cmake
+        pkgs.makeWrapper
+        pythonEnv
+        rustEnv
+      ];
+      # ## This contains the `codesign` utility.
+      # ++ lib.optional pkgs.stdenv.isDarwin pkgs.darwin.security_systemkeychain;
+
+      patches = [
+        ## TODO: This should only apply to the C build, I think.
+        ./reformat-darwin-settings.patch
+        ## TODO: Split this patch – I _think_ some of it only affects the Rust, and other parts only
+        ##       affect the C, so if we adopt cargo2nix or something, it would be good to apply only
+        ##       the relevant parts to each package.
+        ./nix-only.patch
+      ];
+
+      dontConfigure = true;
+
+      buildPhase = ''
+        runHook preBuild
+        ## Since it calls Cargo directly, we have to give it someplace to put everything.
+        CARGO_HOME=$TMPDIR/.cargo \
+          python3 build.py --options simulator --type ${lib.escapeShellArg firmware}
+        runHook postBuild
+      '';
+
+      installPhase = ''
+        runHook preInstall
+        mkdir -p "$out/bin"
+        cp build/simulator "$out/bin/${pname}"
+        mkdir -p "$out/share/ui_simulator"
+        cp -r ui_simulator/assets "$out/share/ui_simulator"
+        runHook postInstall
+      '';
+
+      postFixup = ''
+        ## So it sees ui_simulator/ in the package, rather than looking for it in the directory it
+        ## was run from.
+        wrapProgram "$out/bin/${pname}" --chdir "$out/share"
+      '';
+
+      meta.mainProgram = pname;
+    }
